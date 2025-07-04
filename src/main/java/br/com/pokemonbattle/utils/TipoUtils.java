6d1e36655f071b65b6242dddff820dc3674d275b
Bernard301094 <bernard30101994@gmail.com>
2025-07-04 20:43:51 -0300

    add method to calculate type effectiveness multipliers

--- src/main/java/br/com/pokemonbattle/utils/TipoUtils.java
+++ src/main/java/br/com/pokemonbattle/utils/TipoUtils.java
@@ -1,4 +1,21 @@
 package main.java.br.com.pokemonbattle.utils;
 
+import main.java.br.com.pokemonbattle.enums.TipoPokemon;
+
 public class TipoUtils {
+    public double getMultiplicador(TipoPokemon tipoAtacante, TipoPokemon tipoDefensor) {
+        if (tipoAtacante.equals(tipoDefensor)) {
+            return 0.5; // Mesmo tipo
+        }
+        switch (tipoAtacante) {
+            case TipoPokemon.FOGO:
+                return tipoDefensor.equals(TipoPokemon.GRAMA) ? 2.0 : 0.5;
+            case TipoPokemon.AGUA:
+                return tipoDefensor.equals(TipoPokemon.FOGO) ? 2.0 : 0.5;
+            case TipoPokemon.GRAMA:
+                return tipoDefensor.equals(TipoPokemon.AGUA) ? 2.0 : 0.5;
+            default:
+                return 1.0; // Tipo neutro
+        }
+    }
 }
