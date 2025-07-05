1dcac69c31c4f2b7b423bd57aec3a73d2a434cbd
Bernard301094 <bernard30101994@gmail.com>
2025-07-04 21:02:55 -0300

    add utility methods for reading text and integers from input

--- src/main/java/br/com/pokemonbattle/utils/ScannerUtils.java
+++ src/main/java/br/com/pokemonbattle/utils/ScannerUtils.java
@@ -1,4 +1,15 @@
 package main.java.br.com.pokemonbattle.utils;
 
 public class ScannerUtils {
+    public String lerTexto() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String texto = scanner.nextLine();
+        return texto;
+    }
+
+    public int lerInteiro() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int numero = scanner.nextInt();
+        return numero;
+    }
 }
