8703cc1568451a88e0a4330dadcd1b8656df7b5f
Bernard301094 <bernard30101994@gmail.com>
2025-07-04 21:03:01 -0300

    add name and pokemon fields to Jogador class

--- src/main/java/br/com/pokemonbattle/player/Jogador.java
+++ src/main/java/br/com/pokemonbattle/player/Jogador.java
@@ -1,4 +1,8 @@
 package main.java.br.com.pokemonbattle.player;
 
+import main.java.br.com.pokemonbattle.model.Pokemon;
+
 public class Jogador {
+    private String name;
+    private Pokemon pokemon;
 }
