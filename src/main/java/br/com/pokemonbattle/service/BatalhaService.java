681ff0e802c48e883083804e5c836cf86644516c
Bernard301094 <bernard30101994@gmail.com>
2025-07-04 21:03:08 -0300

    add jogador fields to BatalhaService for player management

--- src/main/java/br/com/pokemonbattle/service/BatalhaService.java
+++ src/main/java/br/com/pokemonbattle/service/BatalhaService.java
@@ -1,4 +1,8 @@
 package main.java.br.com.pokemonbattle.service;
 
+import main.java.br.com.pokemonbattle.player.Jogador;
+
 public class BatalhaService {
+    private Jogador jogador1;
+    private Jogador jogador2;
 }
